27d2d3ea28acdcad1e001079b6d110908f752560
jea7982@naver.com <jea7982@naver.com>
2021-03-18 00:53:29 +0900

    Calculator Application 04

--- app/src/main/java/com/example/calculatorapplication/MainActivity.java
+++ app/src/main/java/com/example/calculatorapplication/MainActivity.java
@@ -2,14 +2,16 @@ package com.example.calculatorapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    boolean inFirstInput = true; //입력 값이 처음 입력되는가를 체크 (기본변수)
+    boolean isFirstInput = true; //입력 값이 처음 입력되는가를 체크 (기본변수)
     int resultNumber = 0; //계산된 결과 값을 저장하는 변수
     char operator = '+'; //입력된 연산자를 저장하는 변수
 
@@ -25,10 +27,41 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void ButtonClick(View view) {
-        if(view.getId() == R.id.num_1_button){
-            resultText.setText("1");
-        } else {
 
+        Button getButton = findViewById(view.getId()); //버튼 생성
+
+
+        switch(getButton.getId()) {
+            case R.id.all_clear_button:
+                isFirstInput = true;
+                resultNumber = 0;
+                operator = '+';
+                resultText.setTextColor(0xFFA8A8A8);
+                resultText.setText(String.valueOf(resultNumber)); //string으로 resultNumber를
+                break;
+
+            case R.id.num_0_button:
+            case R.id.num_1_button:
+            case R.id.num_2_button:
+            case R.id.num_3_button:
+            case R.id.num_4_button:
+            case R.id.num_5_button:
+            case R.id.num_6_button:
+            case R.id.num_7_button:
+            case R.id.num_8_button:
+            case R.id.num_9_button:
+                if (isFirstInput) {
+                    resultText.setTextColor(0xFF000000); //16진수로
+                    resultText.setText(getButton.getText().toString()); //setText 처음 입력되는 경우
+                    isFirstInput = false;
+                } else {
+                    resultText.append(getButton.getText().toString()); //append 추가 되어 입력 (처음 입력되는 경우 x)
+                }
+                break;
+
+            default: //정의 되지 않은 버튼을 눌렀을 때
+                Toast.makeText(getApplicationContext(),getButton.getText().toString() + " 버튼이 클릭되었습니다.", Toast.LENGTH_LONG).show();
+                break;
         }
     }
 }
